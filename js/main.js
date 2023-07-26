7616653d786a081b2967d4d63b05327025fc3349
EdogawaConan2023 <developervn01072023@gmail.com>
2023-07-26 09:24:13 +0700

    ok

--- js/main.js
+++ js/main.js
@@ -1,34 +1,43 @@
-// Định nghĩa hàm printMessage với tham số là một hàm callback
-function printMessage(message, callback) {
-  console.log("Đang in: " + message);
-  // Giả lập việc in mất 2 giây
-  setTimeout(function() {
-    console.log("Đã in xong: " + message);
-    // Gọi callback để thông báo rằng công việc hoàn thành
-    callback();
-  }, 2000);
-}
+// // Định nghĩa hàm printMessage với tham số là một hàm callback
+// function printMessage(message, callback) {
+//   console.log("Đang in: " + message);
+//   // Giả lập việc in mất 2 giây
+//   setTimeout(function() {
+//     console.log("Đã in xong: " + message);
+//     // Gọi callback để thông báo rằng công việc hoàn thành
+//     callback();
+//   }, 2000);
+// }
 
-// Định nghĩa hàm callback mà chúng ta sẽ sử dụng
-function donePrinting() {
-  console.log("Hoàn thành!");
-}
+// // Định nghĩa hàm callback mà chúng ta sẽ sử dụng
+// function donePrinting() {
+//   console.log("Hoàn thành!");
+// }
 
-// Gọi hàm printMessage và truyền hàm callback vào
-printMessage("Xin chào!", donePrinting);
+// // Gọi hàm printMessage và truyền hàm callback vào
+// printMessage("Xin chào!", donePrinting);
 
 
 
  
 
-function printMessage(message, callback) {
-  console.log('Dang in :' + message) ;
-  setTimeout (function() {
-    console.log ('da in xong' + message) ;
-    callback();
-  },2000) ;
+// function printMessage(message, callback) {
+//   console.log('Dang in :' + message) ;
+//   setTimeout (function() {
+//     console.log ('da in xong' + message) ;
+//     callback();
+//   },2000) ;
+// }
+// function donePrintin() {
+//   console.log ('hoan thanhh');
+// }
+// printMessage("Xin chào!", donePrinting);
+
+
+let tuoi = 16
+tuoi = parseInt (window.prompt ('Nhap tuoi'))
+if (tuoi >= 16) {
+  document.write ('Du tuoi di tu')
+} else {
+  document.write ('khong du tuoi di tu')
 }
-function donePrintin() {
-  console.log ('hoan thanhh');
-}
-printMessage("Xin chào!", donePrinting);
